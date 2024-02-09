0d1339b78f0ce9078213180a63ca53b0e6ce9bab
abhishek <abhishehgarg2063@gmail.com>
2024-02-09 10:03:18 +0530

    routing method update

--- src/App.svelte
+++ src/App.svelte
@@ -1,52 +1,62 @@
 <script>
-    import { Route, Router,navigate } from "svelte-routing";
-    import Home from "./views/home/home.svelte"
-    import Login from "./views/login/login.svelte"
-    import Register from "./views/register/register.svelte"
-    import Dashboard from "./views/dashboard/dashboard.svelte"
-    import { private_route, public_routes } from "./routes/app_routes";
-    import { getToken } from "./utils/storage/token";
-    import PublicLayout from "./layouts/PublicLayout.svelte";
-    import PrivateLayout from "./layouts/PrivateLayout.svelte";
- // Check if the access token exists in the session storage
- const accessToken = getToken();
+  import { Route, Router, navigate } from "svelte-routing";
+  import Home from "./views/home/home.svelte";
+  import Login from "./views/login/login.svelte";
+  import Register from "./views/register/register.svelte";
+  import Dashboard from "./views/dashboard/dashboard.svelte";
+  import { private_route, public_routes } from "./routes/app_routes";
+  import { getToken } from "./utils/storage/token";
+  import PublicLayout from "./layouts/PublicLayout.svelte";
+  import PrivateLayout from "./layouts/PrivateLayout.svelte";
+  // Check if the access token exists in the session storage
+  const accessToken = getToken();
 
-// Function to determine if the user is authenticated
-function isAuthenticated() {
-        return !!accessToken;
-    }
+  // Function to determine if the user is authenticated
+  function isAuthenticated() {
+    return !!accessToken;
+  }
 
-     // Function to check if the current route is a private route
-     function isPrivateRoute(pathname) {
-        return Object.values(private_route).includes(pathname);
-    }
+  // Function to check if the current route is a private route
+  function isPrivateRoute(pathname) {
+    return Object.values(private_route).includes(pathname);
+  }
 
-     // Function to check if the current route is a private route
-     function isPublicRoute(pathname) {
-        return Object.values(public_routes).includes(pathname);
-    }
+  // Function to check if the current route is a private route
+  function isPublicRoute(pathname) {
+    return Object.values(public_routes).includes(pathname);
+  }
 
-     // Redirect to the login page if trying to access a private route without authentication
+  // Redirect to the login page if trying to access a private route without authentication
   if (!isAuthenticated() && isPrivateRoute(window.location.pathname)) {
-    navigate( public_routes.login);
+    navigate(public_routes.login);
   }
 
+  // Redirect to the dashboard if authenticated and trying to access a public route
   if (isAuthenticated() && isPublicRoute(window.location.pathname)) {
-    navigate( private_route.dashboard);
+    navigate(private_route.dashboard);
   }
 
+  // If the route is not found in public routes and not authenticated, redirect to the home page
+  if (!isAuthenticated() && !isPublicRoute(window.location.pathname)) {
+    navigate(public_routes.home);
+  }
+
+  // If the route is not found in private routes and  authenticated, redirect to the dashboard page
+  if (isAuthenticated() && !isPrivateRoute(window.location.pathname)) {
+    navigate(private_route.dashboard);
+  }
 </script>
+
 <Router>
   {#if isAuthenticated()}
-      <PrivateLayout>
-          <Route path={private_route.dashboard} component={Dashboard}/>
-      </PrivateLayout>
+    <PrivateLayout>
+      <Route path={private_route.dashboard} component={Dashboard} />
+    </PrivateLayout>
   {:else}
-      <PublicLayout>
-          <Route path={public_routes.login} component={Login}/>
-          <Route path={public_routes.register} component={Register}/>
-          <Route path={public_routes.home} component={Home}/>
-      </PublicLayout>
+    <PublicLayout>
+      <Route path={public_routes.login} component={Login} />
+      <Route path={public_routes.register} component={Register} />
+      <Route path={public_routes.home} component={Home} />
+    </PublicLayout>
   {/if}
 </Router>
-
